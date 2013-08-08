cb28ac0b4da89cb31f743cbdbb816b07d456c970
Yaniv Kessler <yanivk@gmail.com>
2013-08-08 21:55:33 +0300

    initial

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+var layouts = require('log4js').layouts;
+
+function consoleAppender(layout, consoleType) {
+  layout = layout || layouts.colouredLayout;
+
+  if (consoleType === 'error')  {
+  	
+  	return function(loggingEvent) {
+		console.error(layout(loggingEvent));
+	};	
+
+  } else {
+
+	return function(loggingEvent) {
+		console.log(layout(loggingEvent));
+	};	
+	
+  }
+  
+}
+
+function configure(config) {
+  var layout;
+  if (config.layout) {
+    layout = layouts.layout(config.layout.type, config.layout);
+  }
+  return consoleAppender(layout. config.streamType);
+}
+
+exports.appender = consoleAppender;
+exports.configure = configure;
